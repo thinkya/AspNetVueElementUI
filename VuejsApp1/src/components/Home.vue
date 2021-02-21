643a7eb68b449314752525601acfbd48fe4aa38a
刘涛 <thinkya@hotmail.com>
2021-02-21 22:59:27 +0800

    update

---
+++ VuejsApp1/src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home">
+        <h1>{{ msg }}</h1>
+        <h2>{{message}}</h2>
+        <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a>.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        props: {
+            msg: String
+        },
+        data: {
+            message: "hello"
+        }
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
+
